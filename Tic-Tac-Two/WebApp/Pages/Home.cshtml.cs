9c655afd6f9727b5ca45b18f47a24f3e4b0a8e17
Igor Ustritski <igustr@taltech.ee>
2024-12-09 16:48:43 +0200

    web

---
+++ Tic-Tac-Two/WebApp/Pages/Home.cshtml.cs
@@ -0,0 +1,42 @@
+﻿using Azure.Identity;
+using DAL;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace WebApp.Pages;
+
+public class Home : PageModel
+{
+    private readonly IConfigRepository _configRepository;
+
+    public Home(IConfigRepository configRepository)
+    {
+        _configRepository = configRepository;
+    }
+
+
+    [BindProperty(SupportsGet = true)]
+    public string UserName { get; set; } = default!;
+
+    public SelectList ConfigSelectList { get; set; } = default!;
+
+    [BindProperty]
+    public int ConfigId { get; set; }
+    
+    public IActionResult OnGet()
+    {
+        if (string.IsNullOrEmpty(UserName)) return RedirectToPage("./Index", new { error = "No username provided." });
+
+        ViewData["UserName"] = UserName;
+
+        var selectListData = _configRepository.GetConfigurationNames()
+            .Select(name => new {id = name, value = name})
+            .ToList();
+        
+        ConfigSelectList = new SelectList(selectListData, "id", "value");
+        
+        return Page();
+    }
+}
---
+++ Tic-Tac-Two/WebApp/Pages/PlayGameWeb.cshtml.cs
@@ -0,0 +1,46 @@
+﻿using DAL;
+using GameBrain;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApp.Pages;
+
+public class PlayGameWeb : PageModel
+{
+    private readonly IConfigRepository _configRepository;
+    private readonly IGameRepository _gameRepository;
+
+    public PlayGameWeb(IConfigRepository configRepository, IGameRepository gameRepository)
+    {
+        _configRepository = configRepository;
+        _gameRepository = gameRepository;
+    }
+
+
+    [BindProperty(SupportsGet = true)] public int GameId { get; set; } = default!;
+
+    [BindProperty(SupportsGet = true)] public EGamePiece NextMoveBy { get; set; } = default!;
+
+    public TicTacTwoBrain TicTacToeBrain { get; set; } = default!;
+
+    /*
+    public void OnGet(int? x, int? y)
+    {
+        var dbGame = _gameRepository.LoadGame(GameId);
+        TicTacToeBrain = new TicTacTwoBrain(new GameConfiguration()
+        {
+            Name = "Classical"
+        });
+
+        TicTacToeBrain.SetGameStateJson(dbGame.GameState);
+
+        if (x != null && y != null)
+        {
+            var userGameName = "Game1 (ChangeThat)";
+            TicTacToeBrain.MakeAMove(x.Value, y.Value);
+            GameId = _gameRepository.SaveGame(TicTacToeBrain.GetGameStateJson(),TicTacToeBrain.GetGameConfigName(), userGameName);
+        }
+
+    }
+    */
+}
